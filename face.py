1610af49663979d5b12a335a58e70d56dd0a6cc5
Peter Georgas <pgeorga@rockets.utoledo.edu>
2022-12-05 15:34:06 -0500

    rotate 90 clockwise

--- face.py
+++ face.py
@@ -57,17 +57,12 @@ try:
         face_found = False
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        small_frame = cv2.rotate(small_frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        small_frame = cv2.rotate(small_frame, cv2.ROTATE_90_CLOCKWISE)
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        """
-        if len(face_locations) == 0 and time_elapsed > 15:
-            last_seen = None
-            interval_start_time = time.time()  # Reset the time interval
-            continue
-        """
+
         face_encodings = face_recognition.face_encodings(
             rgb_small_frame, face_locations
         )
